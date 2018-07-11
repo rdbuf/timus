bb5b19cea82f0999d9ff4cd6b7411965878f65ef
Ilya Pikulin <ilya.pikulin@gmail.com>
2018-07-11 22:02:21 +0300

    Solve #1003

--- src/1003.cc
+++ src/1003.cc
@@ -1,60 +1,39 @@
 #include <iostream>
-#include <cassert>
 #include <algorithm>
 #include <unordered_map>
-#include <tuple>
-#include <vector>
 #include <string>
 #include <utility>
 #include <functional>
 #include <cstdint>
 using namespace std;
 
-struct Parity {
-	bool value;
-	bool initialized = false;
-	Parity() = default;
-	constexpr Parity(bool a) : value(a), initialized(true) {}
-	constexpr Parity operator+(Parity rhs) const { assert(rhs.initialized); return Parity(initialized ? value ^ rhs.value : rhs.value); }
-	constexpr bool operator==(Parity rhs) const { return (initialized ? rhs.value == value : true); }
-	constexpr bool operator!=(Parity rhs) const { return !(*this == rhs); }
-};
-constexpr static Parity even(0), odd(1);
-
 int main() {
 	// we only then know for sure when we have the same segment covered in exact two times
 	// thus, at each step we join the new segment with the already existent ones and check for twins with different parity
+
+	// all we need to keep track of the segments is a map: l -> (r, parity) since
+	// if we have several segments starting at l, we can unambiguously transform them into a few adjacent segments
+	// and otherwise we're already safe
 	int n, m;
 	while (cin >> n >> m) {
-		int steps = 0;
-		unordered_map<uint32_t, unordered_map<uint32_t, Parity>> straight, reverse; // segments
-		for ( ; steps < m; ++steps) {
+		unordered_map<uint32_t, uint32_t> segments; // [l; r) segments set
+		unordered_map<uint32_t, bool> segment_parity;
+		function<bool(uint32_t, uint32_t, bool)> insert_segment = [&](uint32_t l, uint32_t r, bool parity) -> bool { // returns the consistency
+			if (segments.find(l) == segments.end()) {
+				segments[l] = r, segment_parity[l] = parity;
+				return true;
+			}
+			if (segments[l] == r) return segment_parity[l] == parity;
+			if (segments[l] < r) parity ^= segment_parity[l];
+			else segment_parity[l] = exchange(parity, parity ^ segment_parity[l]);
+			if (segments[l] > r) swap(segments[l], r);
+			return insert_segment(segments[l], r, parity);
+		};
+		int steps;
+		for (steps = 0; steps < m; ++steps) {
 			uint32_t l, r; string str_parity; cin >> l >> r >> str_parity;
-			if (l > n || r > n) goto exit;
-			Parity parity = (str_parity == "even" ? even : odd);
-
-			if (exchange(straight[l][r], parity) != parity) goto exit;
-			if (exchange(reverse[r][l], parity) != parity) goto exit;
-			for (const auto& x : reverse[l - 1]) {
-				const auto& newseg_l = x.first, & newseg_r = r; const auto newseg_parity = x.second + parity;
-				if (exchange(straight[newseg_l][newseg_r], newseg_parity) != newseg_parity) goto exit;
-				if (exchange(reverse[newseg_r][newseg_l], newseg_parity) != newseg_parity) goto exit;
-				for (const auto& y : straight[r + 1]) {
-					const auto& newseg_r = y.first; const auto newseg_parity = (x.second + parity) + y.second;
-					if (exchange(straight[newseg_l][newseg_r], newseg_parity) != newseg_parity) goto exit;
-					if (exchange(reverse[newseg_r][newseg_l], newseg_parity) != newseg_parity) goto exit;
-				}
-			}
-			for (const auto& x : straight[r + 1]) {
-				const auto& newseg_l = l, & newseg_r = x.first; const auto newseg_parity = parity + x.second;
-				if (exchange(straight[newseg_l][newseg_r], newseg_parity) != newseg_parity) goto exit;
-				if (exchange(reverse[newseg_r][newseg_l], newseg_parity) != newseg_parity) goto exit;
-				for (const auto& y : reverse[l - 1]) {
-					const auto& newseg_l = y.first; const auto newseg_parity = y.second + (parity + x.second);
-					if (exchange(straight[newseg_l][newseg_r], newseg_parity) != newseg_parity) goto exit;
-					if (exchange(reverse[newseg_r][newseg_l], newseg_parity) != newseg_parity) goto exit;
-				}
-			}
+			bool parity = str_parity == "odd";
+			if (!insert_segment(l, r+1, parity)) goto exit;
 		}
 	exit:
 		cout << steps << "\n";
