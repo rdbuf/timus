4ac51ff9dc817394c63671c30183d990ae4e9ce7
Ilya Pikulin <ilya.pikulin@gmail.com>
2018-07-13 18:44:33 +0300

    Enhance #1427

--- src/1427.cc
+++ src/1427.cc
@@ -6,20 +6,19 @@
 #include <numeric>
 using namespace std;
 
+// plain greedy approach
 int main() {
 	int n, m; cin >> n >> m;
 	string msg; getline(cin, msg), getline(cin, msg);
-	const size_t msg_size = msg.length();
-	vector<int> a(msg_size+1, numeric_limits<int>::max());
-	a[0] = 0;
-	for (size_t i = 0; i < msg_size; ) {
-		int range = max<int>(find_if_not(
+	int msgs = 0;
+	for (
+		size_t i = 0;
+		i < msg.length();
+		i += max<int>(find_if_not(
 				min(msg.end(), msg.begin() + i),
 				min(msg.end(), msg.begin() + i + m),
 				[](const char c) { return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == ' '; }
-			) - min(msg.end(), msg.begin() + i), n);
-		for (size_t j = i + 1; j < i + range + 1 && j < msg_size+1; ++j) a[j] = min(a[j], a[i] + 1);
-		i += range;
-	}
-	cout << a[msg_size];
+			) - min(msg.end(), msg.begin() + i), n)
+	) msgs += 1;
+	cout << msgs;
 }
