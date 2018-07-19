86e314e29e72e84d26a186eda0c259e83745f464
Ilya Pikulin <ilya.pikulin@gmail.com>
2018-07-19 20:21:18 +0300

    Clean up #2099

--- src/2099.hs
+++ src/2099.hs
@@ -1,8 +1,9 @@
-import Data.Function
 import Control.Applicative
 import Data.Monoid
 import Data.List
 import Data.Ratio
+
+deleteAt idx = (<>) <$> take idx <*> drop (idx + 1)
 
 (<.>) = (sum .) . zipWith (*)
 (<->) = zipWith (-)
@@ -17,18 +18,16 @@ det matrix | not . all ((== length matrix) . length) $ matrix = Nothing
     submatrices = map ($ transpose . tail $ matrix) (map deleteAt [0..dim-1])
     dim = length . head $ matrix
 
-projectionK target source = k where
-    k = (source <.> target) % (target <.> target)
-
-deleteAt idx = (<>) <$> take idx <*> drop (idx + 1)
-
 orthogonal a b = a <.> b == 0
 complanar a b c d = det [b <-> a, c <-> a, d <-> a] == Just 0
+projectionCoeff target source = (source <.> target) % (target <.> target)
 
 main = do
     [a,b,c,d] <- map (map read . words) . lines <$> getContents
-    let valid = orthogonal (b <-> a) (d <-> c) && complanar a b c d && validateCoordinates where
-        validateCoordinates = c' !! 0 >= b' !! 0 && (c' !! 1 >= 0) == (d' !! 1 >= 0) && abs (c' !! 1) < abs (d' !! 1)
-        (a', b', c', d') = ([0, 0], [1, 0], [projectionK (b <-> a) (c <-> a), projectionK (d <-> c) (c <-> a)], c' <+> [0, 1])
+    let valid = orthogonal (b <-> a) (d <-> c) && complanar a b c d && coordinatesValid where
+        coordinatesValid = c' !! 0 >= b' !! 0 && (c' !! 1 >= 0) == (d' !! 1 >= 0) && abs (c' !! 1) < abs (d' !! 1)
+        a' = [0, 0]
+        b' = [1, 0]
+        c' = [projectionCoeff (b <-> a) (c <-> a), projectionCoeff (d <-> c) (c <-> a)]
+        d' = c' <+> [0, 1]
     putStr $ if valid then "Valid" else "Invalid"
-    return ()
